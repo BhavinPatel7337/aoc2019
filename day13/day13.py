359c0acd499e29de4529d1ae3702f963be4bd4d5
BhavinPatel7337 <bhavin.patel7337@gmail.com>
2019-12-13 16:31:45 +0000

    Refactored day 13

--- day13/day13.py
+++ day13/day13.py
@@ -3,32 +3,29 @@ path.append(path[0] + "/..")
 from intcode import IntCode
 
 def display(output):
-    screen = {}
-    for i in range(0, len(output), 3):
-        screen[(output[i], output[i + 1])] = output[i + 2]
-
+    sprites = {
+        0: ' ',
+        1: '\u2588',
+        2: '#',
+        3: '@',
+        4: '0'
+    }
+    screen = {(output[i], output[i + 1]): output[i + 2] for i in range(0, len(output), 3)}
     maxX = max(k[0] for k in screen.keys())
     maxY = max(k[1] for k in screen.keys())
     canvas = ''
     for y in range(maxY + 1):
         for x in range(maxX + 1):
             if (x, y) in screen:
-                if screen[(x,y)] == 0:
-                    canvas += ' '
-                elif screen[(x,y)] == 1:
-                    canvas += '\u2588'
-                elif screen[(x,y)] == 2:
-                    canvas += '#'
-                elif screen[(x,y)] == 3:
-                    canvas += '@'
-                elif screen[(x,y)] == 4:
-                    canvas += '0'
+                canvas += sprites[screen[(x, y)]]
             else:
                 canvas += ' '
         canvas += '\n'
     print(canvas[:-1])
     return screen
 
+def compare(a, b):
+    return 0 if b - a == 0 else int((b - a) / abs(b - a))
 
 with open(path[0] + "/input.txt") as f:
     program = [int(x) for x in f.read().strip().split(',')]
@@ -41,12 +38,7 @@ part1 = list(screen.values()).count(2)
 while not arcade.halted:
     ball = list(screen.keys())[list(screen.values()).index(4)]
     paddle = list(screen.keys())[list(screen.values()).index(3)]
-    if ball[0] < paddle[0]:
-        arcade.run(-1)
-    elif ball[0] > paddle[0]:
-        arcade.run(1)
-    else:
-        arcade.run(0)
+    output = arcade.run(compare(paddle[0], ball[0]))
     screen = display(output)
 
 print(part1)
